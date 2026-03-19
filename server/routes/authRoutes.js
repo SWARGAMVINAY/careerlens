0c6732e52b989e24a94c4e261b8f612df4b21946
SWARGAMVINAY <vinaysmiley912@gmail.com>
2026-03-19 17:11:02 +0530

    complete UI improvements before deployment

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,11 +2,17 @@ const express = require('express')
 const router = express.Router()
 const { registerUser, loginUser } = require('../controllers/authController')
 const protect = require('../middleware/authMiddleware')
+const User = require('../models/User')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
-router.get('/me', protect, (req, res) => {
-    res.json({ message: 'You are logged in', userId: req.user.id })
+router.get('/me', protect, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' })
+    }
 })
 
 module.exports = router
